1c2dbe27e93d88a1fd373ec4a368add164fb5cc4
Dave Ross <dave@davidmichaelross.com>
2014-03-07 21:53:49 -0500

    Code formatting

--- upgrade-first-aid.php
+++ upgrade-first-aid.php
@@ -46,7 +46,7 @@ class UpgradeFirstAid {
 	 */
 	public static function plugin_options() {
 
-		if ( !class_exists( 'WP_Filesystem_Direct' ) ) {
+		if ( ! class_exists( 'WP_Filesystem_Direct' ) ) {
 			require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php';
 			require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-direct.php';
 		}
@@ -60,14 +60,14 @@ class UpgradeFirstAid {
 
 		echo '<div class="details">';
 		echo '<h3>' . __( 'Core Upgrades', 'upgrade_first_aid' ) . '</h3>';
-		$type = get_filesystem_method( array(), ABSPATH );
+		$type   = get_filesystem_method( array(), ABSPATH );
 		$method = UpgradeFirstAidUtil::upgrade_method_description( $type );
 		echo '<p>' . UpgradeFirstAidUtil::type_icon( $type ) . ' ' . sprintf( 'WordPress can install core upgrades %s.', $method ) . '</p>';
 		self::maybe_disk_full( ABSPATH );
 		self::maybe_ownership_mismatch( ABSPATH . 'index.php' );
 
-		echo '<h3>' . __( 'Plugin Upgrades' , 'upgrade_first_aid' ) . '</h3>';
-		$type = get_filesystem_method( array(), WP_PLUGIN_DIR );
+		echo '<h3>' . __( 'Plugin Upgrades', 'upgrade_first_aid' ) . '</h3>';
+		$type   = get_filesystem_method( array(), WP_PLUGIN_DIR );
 		$method = UpgradeFirstAidUtil::upgrade_method_description( $type );
 		echo '<p>' . UpgradeFirstAidUtil::type_icon( $type ) . sprintf( 'WordPress can install plugins %s.', $method ) . '</p>';
 		self::maybe_disk_full( WP_PLUGIN_DIR );
@@ -76,14 +76,14 @@ class UpgradeFirstAid {
 		$all_plugins = get_plugins();
 		foreach ( $all_plugins as $plugin_path => $plugin_details ) {
 			$plugin_full_path = dirname( WP_PLUGIN_DIR . "/{$plugin_path}" );
-			$type = get_filesystem_method( array(), WP_PLUGIN_DIR );
-			$method = UpgradeFirstAidUtil::upgrade_method_description( $type );
+			$type             = get_filesystem_method( array(), WP_PLUGIN_DIR );
+			$method           = UpgradeFirstAidUtil::upgrade_method_description( $type );
 			echo '<p>' . UpgradeFirstAidUtil::type_icon( $type ) . sprintf( 'WordPress can upgrade the %s plugin %s.', $plugin_details['Name'], $method ) . '</p>';
 			self::maybe_ownership_mismatch( $plugin_full_path, $plugin_details['Name'] );
 		}
 
 		echo '<h3>' . __( 'Theme Upgrades', 'upgrade_first_aid' ) . '</h3>';
-		$type = get_filesystem_method( array(), get_theme_root() );
+		$type   = get_filesystem_method( array(), get_theme_root() );
 		$method = UpgradeFirstAidUtil::upgrade_method_description( $type );
 		echo '<p>' . UpgradeFirstAidUtil::type_icon( $type ) . sprintf( 'WordPress can install themes %s.', $method ) . '</p>';
 		self::maybe_disk_full( get_theme_root() );
@@ -95,8 +95,8 @@ class UpgradeFirstAid {
 
 		foreach ( $all_themes as $theme_path => $theme_details ) {
 			$theme_full_path = $theme_details->get_stylesheet_directory();
-			$type = get_filesystem_method( array(), $theme_full_path );
-			$method = UpgradeFirstAidUtil::upgrade_method_description( $type );
+			$type            = get_filesystem_method( array(), $theme_full_path );
+			$method          = UpgradeFirstAidUtil::upgrade_method_description( $type );
 			echo '<p>' . UpgradeFirstAidUtil::type_icon( $type ) . sprintf( 'WordPress can upgrade the %s theme %s.', $theme_details->Name, $method ) . '</p>';
 			self::maybe_ownership_mismatch( get_theme_root() );
 		}
@@ -113,12 +113,13 @@ class UpgradeFirstAid {
 	 * Check for a possible file ownership mismatch and display an error
 	 *
 	 * @author Dave Ross <dave@davidmichaelross.com>
-	 * @param string  $context directory whose permissions should be checked
+	 *
+	 * @param string $context directory whose permissions should be checked
 	 */
 	private static function maybe_ownership_mismatch( $context, $item = "WordPress" ) {
-		$php_user = UpgradeFirstAidUtil::current_php_user();
+		$php_user             = UpgradeFirstAidUtil::current_php_user();
 		$wp_filesystem_direct = new WP_Filesystem_Direct( null );
-		$wp_owner = $wp_filesystem_direct->owner( $context );
+		$wp_owner             = $wp_filesystem_direct->owner( $context );
 
 		if ( preg_match( '/index.php$/', $context ) ) {
 			$directory = dirname( $context );
@@ -129,7 +130,7 @@ class UpgradeFirstAid {
 
 		if ( $php_user !== $wp_owner ) {
 			echo '<p>' . UpgradeFirstAidUtil::img_tag( admin_url( 'images/no.png' ) ) . sprintf( __( "PHP is currently running as the user <em>%s</em>, but the %s files are owned by the user <em>%s</em>. WordPress could install upgrades without FTP if you changed the files' owner to <em>%s</em>.", 'upgrade_first_aid' ), $php_user, $item, $wp_owner, $php_user ) . '</p>';
-			if ( UpgradeFirstAidUtil::can_write_to_directory( $directory ) && !defined( 'FS_METHOD' ) ) {
+			if ( UpgradeFirstAidUtil::can_write_to_directory( $directory ) && ! defined( 'FS_METHOD' ) ) {
 				echo '<p>' . UpgradeFirstAidUtil::img_tag( admin_url( 'images/comment-grey-bubble.png' ) ) . sprintf( __( "<em>%s</em> can write to the %s directory, so you can try adding <code>%s</code> to your wp-config.php file which might allow upgrades without FTP.", 'upgrade_first_aid' ), $php_user, $directory, "define('FS_METHOD', 'direct');" ) . '</p>';
 			}
 		}
@@ -139,7 +140,8 @@ class UpgradeFirstAid {
 	 * Display an error if the disk is low on free space
 	 *
 	 * @author Dave Ross <dave@davidmichaelross.com>
-	 * @param string  $context directory whose partition should be checked
+	 *
+	 * @param string $context directory whose partition should be checked
 	 */
 	private static function maybe_disk_full( $context ) {
 		$free_space = disk_free_space( $context );
@@ -154,7 +156,7 @@ class UpgradeFirstAid {
 		echo '</ul>';
 	}
 
-	public static function error_handler( $errno , $errstr,  $errfile , $errline ,  $errcontext ) {
+	public static function error_handler( $errno, $errstr, $errfile, $errline, $errcontext ) {
 		print "$errno $errstr $errfile $errline";
 		print "<hr>";
 		print_r( $errcontext );
